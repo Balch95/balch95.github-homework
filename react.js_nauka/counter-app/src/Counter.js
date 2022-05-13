c02d3c56379db9a07831d5c751b34598eeae16c2
Balch95 <lukasz.wozniak95@hotmail.com>
2022-05-13 22:31:41 +0200

    push

---
+++ react.js_nauka/counter-app/src/Counter.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './Counter.css';
+
+
+class Counter extends Component {
+    render(){
+
+        let randomNumber  = Math.floor(Math.random() * (108-1+1) +1 );
+
+        return(
+            <div className='counter'>
+            Counter:
+                <span className='value'>
+                    {randomNumber}
+                </span>
+            </div>
+        );
+    };
+};
+
+// function Counter(props){
+
+    // let randomNumber  = Math.floor(Math.random() * (108-1+1) +1 );
+//     return (
+//         <div className='counter'>
+//             Counter:
+//             <span className='value'>
+//                 {randomNumber}
+//             </span>
+//         </div>
+//     );
+// }
+
+export default Counter;
